fd206be845ca3e231f729dc09fa0c5fe9043cd7a
Daniele Marighetti Cox <daniele.marighetti@gmail.com>
2022-03-01 11:31:23 -0300

    Repetição otimizada com For

--- main.js
+++ main.js
@@ -4,25 +4,14 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-
     const instrumento = tecla.classList[1];
-
-    // Template String
-
-    const idAudio = `#som_${instrumento}`;
-
-    console.log(idAudio);
+    const idAudio = `#som_${instrumento}`; // Template String
 
     tecla.onclick = function() {
-
         tocaSom(idAudio);
     };
-    contador = contador + 1;
 
-    console.log(contador);
 }
