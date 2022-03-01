d7a4fa0695454f3acd28bf3f3f867752357694c3
Daniele Marighetti Cox <daniele.marighetti@gmail.com>
2022-03-01 09:17:47 -0300

    trabalhando com listas

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
+// Pom ----------------------------------------
 function tocaSomPom() {
-
     document.querySelector('#som_tecla_pom').play();
 }
 
-function tocaSomClap() {
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-    document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_pom');
+listaDeTeclas[0].onclick = tocaSomPom;
