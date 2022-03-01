e4d680eee268e76571aaf2cfa89c7a01dba660d9
Daniele Marighetti Cox <daniele.marighetti@gmail.com>
2022-03-01 15:34:21 -0300

    Projeto otimizado

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; // Template String
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function() {
         tocaSom(idAudio);
-    };
-    tecla.onKeyDown = function(evento) {
-        console.log(evento.code == 'Space');
-        if (evento.code === 'Space') {
+    }
+
+    tecla.onkeydown = function(evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
 
-    };
-    tecla.onKeyUp = function() {
-        tecla.classList.remove('ativa');
     }
 
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
 
 }
