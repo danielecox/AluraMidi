1154a8b17805a96085b09a7aa9787f8f5c19b02c
Daniele Marighetti Cox <daniele.marighetti@gmail.com>
2022-03-01 11:21:44 -0300

    Textos dinâmicos

--- main.js
+++ main.js
@@ -7,9 +7,22 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = function() {
-        tocaSom('#som_tecla_pom');
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    // Template String
+
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio);
+
+    tecla.onclick = function() {
+
+        tocaSom(idAudio);
     };
     contador = contador + 1;
+
     console.log(contador);
 }
